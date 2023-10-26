249f2f1f7a4e645ab0782c3ebcf57f5c8e655c3f
Afonso Corte-Real <afonsocortereal15@gmail.com>
2023-10-26 23:15:33 +0100

    Add comments to Student Selector function

--- js/student-selector-script.js
+++ js/student-selector-script.js
@@ -1,21 +1,32 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
+// This function toggles the visibility of the dropdown menu.
 function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
+  // Get the dropdown element.
+  const dropdown = document.getElementById("myDropdown");
+
+  // Toggle the "show" class.
+  dropdown.classList.toggle("show");
 }
 
+// This function filters the dropdown menu items based on the input value.
 function filterFunction() {
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown");
-  a = div.getElementsByTagName("a");
-  for (i = 0; i < a.length; i++) {
-    txtValue = a[i].textContent || a[i].innerText;
-    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      a[i].style.display = "";
+  // Get the input and filter values.
+  const input = document.getElementById("myInput");
+  const filter = input.value.toUpperCase();
+
+  // Get the dropdown element and its items.
+  const dropdown = document.getElementById("myDropdown");
+  const items = dropdown.getElementsByTagName("a");
+
+  // Loop through the items and filter them.
+  for (let i = 0; i < items.length; i++) {
+    // Get the item text.
+    const itemText = items[i].textContent || items[i].innerText;
+
+    // If the item text contains the filter value, show it. Otherwise, hide it.
+    if (itemText.toUpperCase().indexOf(filter) > -1) {
+      items[i].style.display = "";
     } else {
-      a[i].style.display = "none";
+      items[i].style.display = "none";
     }
   }
 }
