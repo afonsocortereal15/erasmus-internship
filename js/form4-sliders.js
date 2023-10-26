6f1b853b221117e64124893db9e82f336bfd75d0
Afonso Corte-Real <afonsocortereal15@gmail.com>
2023-10-26 23:15:08 +0100

    Add comments to Sliders function

--- js/form4-sliders.js
+++ js/form4-sliders.js
@@ -1,3 +1,5 @@
+// This function updates the value of an input element with the given ID.
 function outputUpdate(inputId, outputValue) {
+  // Get the input element with the given ID and set the value of the input element to the given output value.
   document.querySelector('#' + inputId).value = outputValue;
 }
